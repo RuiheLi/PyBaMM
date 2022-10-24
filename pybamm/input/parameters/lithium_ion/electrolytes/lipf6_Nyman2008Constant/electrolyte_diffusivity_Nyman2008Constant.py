f47f35253c46930a0d416739f415e07304d2ae0b
RuiheLi <lrhkml@foxmail.com>
2022-10-25 00:10:48 +0100

    update 221025 office- back to constant nyman

---
+++ pybamm/input/parameters/lithium_ion/electrolytes/lipf6_Nyman2008Constant/electrolyte_diffusivity_Nyman2008Constant.py
@@ -0,0 +1,35 @@
+from pybamm import exp
+def electrolyte_diffusivity_Nyman2008Constant(c_e,c_EC, T):
+    """
+    Diffusivity of LiPF6 in EC:EMC (3:7) as a function of ion concentration. The data
+    comes from [1]
+
+    References
+    ----------
+    .. [1] A. Nyman, M. Behm, and G. Lindbergh, "Electrochemical characterisation and
+    modelling of the mass transport phenomena in LiPF6-EC-EMC electrolyte,"
+    Electrochim. Acta, vol. 53, no. 22, pp. 6356–6365, 2008.
+
+    Parameters
+    ----------
+    c_e: :class:`pybamm.Symbol`
+        Dimensional electrolyte concentration
+    T: :class:`pybamm.Symbol`
+        Dimensional temperature
+
+    Returns
+    -------
+    :class:`pybamm.Symbol`
+        Solid diffusivity
+    """
+
+    D_c_e = (
+        (c_e <= 2000) * (
+            8.794e-11 * (c_e / 1000) ** 2 - 3.972e-10 * (c_e / 1000) + 4.862e-10) 
+        + (c_e > 2000) * (
+        8.794e-11 * (2000 / 1000) ** 2 - 3.972e-10 * (2000 / 1000) + 4.862e-10)
+    )
+
+    # Nyman et al. (2008) does not provide temperature dependence
+
+    return D_c_e
