e1756abb23e77bd70a7209f9e91e276fb88c1dd4
DrSOKane <s.okane@imperial.ac.uk>
2023-01-11 12:44:54 +0000

    Throughput capacity is now always calculated even if calculate discharge energy is set to false

--- pybamm/models/submodels/external_circuit/base_external_circuit.py
+++ pybamm/models/submodels/external_circuit/base_external_circuit.py
@@ -12,17 +12,18 @@ class BaseModel(pybamm.BaseSubModel):
 
     def get_fundamental_variables(self):
         Q_Ah = pybamm.standard_variables.Q_Ah
-
-        variables = {"Discharge capacity [A.h]": Q_Ah}
+        Qt_Ah = pybamm.standard_variables.Qt_Ah
+        variables = {
+            "Discharge capacity [A.h]": Q_Ah,
+            "Throughput capacity [A.h]": Qt_Ah,
+        }
         if self.options["calculate discharge energy"] == "true":
             Q_Wh = pybamm.standard_variables.Q_Wh
             Qt_Wh = pybamm.standard_variables.Qt_Wh
-            Qt_Ah = pybamm.standard_variables.Qt_Ah
             variables.update(
                 {
                     "Discharge energy [W.h]": Q_Wh,
                     "Throughput energy [W.h]": Qt_Wh,
-                    "Throughput capacity [A.h]": Qt_Ah,
                 }
             )
         else:
@@ -30,36 +31,34 @@ class BaseModel(pybamm.BaseSubModel):
                 {
                     "Discharge energy [W.h]": pybamm.Scalar(0),
                     "Throughput energy [W.h]": pybamm.Scalar(0),
-                    "Throughput capacity [A.h]": pybamm.Scalar(0),
                 }
             )
         return variables
 
     def set_initial_conditions(self, variables):
         Q_Ah = variables["Discharge capacity [A.h]"]
+        Qt_Ah = variables["Throughput capacity [A.h]"]
         self.initial_conditions[Q_Ah] = pybamm.Scalar(0)
+        self.initial_conditions[Qt_Ah] = pybamm.Scalar(0)
         if self.options["calculate discharge energy"] == "true":
             Q_Wh = variables["Discharge energy [W.h]"]
             Qt_Wh = variables["Throughput energy [W.h]"]
-            Qt_Ah = variables["Throughput capacity [A.h]"]
             self.initial_conditions[Q_Wh] = pybamm.Scalar(0)
             self.initial_conditions[Qt_Wh] = pybamm.Scalar(0)
-            self.initial_conditions[Qt_Ah] = pybamm.Scalar(0)
 
     def set_rhs(self, variables):
         # ODEs for discharge capacity and throughput capacity
         Q_Ah = variables["Discharge capacity [A.h]"]
+        Qt_Ah = variables["Throughput capacity [A.h]"]
         I = variables["Current [A]"]
-
         self.rhs[Q_Ah] = I * self.param.timescale / 3600
+        self.rhs[Qt_Ah] = abs(I) * self.param.timescale / 3600
         if self.options["calculate discharge energy"] == "true":
             Q_Wh = variables["Discharge energy [W.h]"]
             Qt_Wh = variables["Throughput energy [W.h]"]
-            Qt_Ah = variables["Throughput capacity [A.h]"]
             V = variables["Terminal voltage [V]"]
             self.rhs[Q_Wh] = I * V * self.param.timescale / 3600
             self.rhs[Qt_Wh] = abs(I * V) * self.param.timescale / 3600
-            self.rhs[Qt_Ah] = abs(I) * self.param.timescale / 3600
 
 
 class LeadingOrderBaseModel(BaseModel):
@@ -70,16 +69,18 @@ class LeadingOrderBaseModel(BaseModel):
 
     def get_fundamental_variables(self):
         Q_Ah = pybamm.Variable("Leading-order discharge capacity [A.h]")
-        variables = {"Discharge capacity [A.h]": Q_Ah}
+        Qt_Ah = pybamm.Variable("Leading-order throughput capacity [A.h]")
+        variables = {
+            "Discharge capacity [A.h]": Q_Ah,
+            "Throughput capacity [A.h]": Qt_Ah,
+        }
         if self.options["calculate discharge energy"] == "true":
             Q_Wh = pybamm.Variable("Leading-order discharge energy [W.h]")
             Qt_Wh = pybamm.Variable("Leading-order throughput energy [W.h]")
-            Qt_Ah = pybamm.Variable("Leading-order throughput capacity [A.h]")
             variables.update(
                 {
                     "Discharge energy [W.h]": Q_Wh,
                     "Throughput energy [W.h]": Qt_Wh,
-                    "Throughput capacity [A.h]": Qt_Ah,
                 }
             )
         return variables
