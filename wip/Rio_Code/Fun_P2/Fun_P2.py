5804dc4e62ac89fc6174d3f24665bebcc7be5456
RuiheLi <lrhkml@foxmail.com>
2023-03-04 09:41:12 +0000

    add Landes_constant

--- wip/Rio_Code/Fun_P2/Fun_P2.py
+++ wip/Rio_Code/Fun_P2/Fun_P2.py
@@ -12,25 +12,173 @@ import openpyxl
 import traceback
 import random;import time, signal
 
-def electrolyte_conductivity_base_Landesfeind2019(c_e, T, coeffs):
-    c = c_e / 1000  # mol.m-3 -> mol.l
+# define Landesfeind exp(initial) and constant 
+def electrolyte_conductivity_base_Landesfeind2019_Constant(c_e, T, coeffs):
+    # mol.m-3 -> mol.l
+    c = (c_e<4000)*c_e / 1000 +  (c_e>4000)* 4 # Mark Ruihe 
     p1, p2, p3, p4, p5, p6 = coeffs
     A = p1 * (1 + (T - p2))
-    B = 1 + p3 * pybamm.sqrt(c) + p4 * (1 + p5 * pybamm.exp(1000 / T)) * c
-    C = 1 + c**4 * (p6 * pybamm.exp(1000 / T))
+    B = 1 + p3 * pb.sqrt(c) + p4 * (1 + p5 * pb.exp(1000 / T)) * c
+    C = 1 + c ** 4 * (p6 * pb.exp(1000 / T))
+    sigma_e = A * c * B / C  # mS.cm-1
+
+    return sigma_e / 10
+
+def electrolyte_diffusivity_base_Landesfeind2019_Constant(c_e, T, coeffs):
+    # mol.m-3 -> mol.l
+    c = (c_e<4000)*c_e / 1000 +  (c_e>4000)* 4 # Mark Ruihe 
+    p1, p2, p3, p4 = coeffs
+    A = p1 *pb.exp(p2 * c)
+    B = pb.exp(p3 / T)
+    C = pb.exp(p4 * c / T)
+    D_e = A * B * C * 1e-10  # m2/s
+
+    return D_e
+
+def electrolyte_TDF_base_Landesfeind2019_Constant(c_e, T, coeffs):
+    c = (c_e<4000)*c_e / 1000 +  (c_e>4000)* 4 # Mark Ruihe 
+    p1, p2, p3, p4, p5, p6, p7, p8, p9 = coeffs
+    tdf = (
+        p1
+        + p2 * c
+        + p3 * T
+        + p4 * c ** 2
+        + p5 * c * T
+        + p6 * T ** 2
+        + p7 * c ** 3
+        + p8 * c ** 2 * T
+        + p9 * c * T ** 2
+    )
+    return tdf
+
+def electrolyte_transference_number_base_Landesfeind2019_Constant(c_e, T, coeffs):
+    c = (c_e<4000)*c_e / 1000 +  (c_e>4000)* 4 # Mark Ruihe 
+    p1, p2, p3, p4, p5, p6, p7, p8, p9 = coeffs
+    tplus = (
+        p1
+        + p2 * c
+        + p3 * T
+        + p4 * c ** 2
+        + p5 * c * T
+        + p6 * T ** 2
+        + p7 * c ** 3
+        + p8 * c ** 2 * T
+        + p9 * c * T ** 2
+    )
+
+    return tplus
+def electrolyte_transference_number_EC_EMC_3_7_Landesfeind2019_Constant(c_e, T):
+    coeffs = np.array(
+        [
+            -1.28e1,
+            -6.12,
+            8.21e-2,
+            9.04e-1,
+            3.18e-2,
+            -1.27e-4,
+            1.75e-2,
+            -3.12e-3,
+            -3.96e-5,
+        ]
+    )
+
+    return electrolyte_transference_number_base_Landesfeind2019_Constant(c_e, T, coeffs)
+
+def electrolyte_TDF_EC_EMC_3_7_Landesfeind2019_Constant(c_e, T):
+    coeffs = np.array(
+        [2.57e1, -4.51e1, -1.77e-1, 1.94, 2.95e-1, 3.08e-4, 2.59e-1, -9.46e-3, -4.54e-4]
+    )
+
+    return electrolyte_TDF_base_Landesfeind2019_Constant(c_e, T, coeffs)
+
+def electrolyte_diffusivity_EC_EMC_3_7_Landesfeind2019_Constant(c_e, T):
+    coeffs = np.array([1.01e3, 1.01, -1.56e3, -4.87e2])
+
+    return electrolyte_diffusivity_base_Landesfeind2019_Constant(c_e, T, coeffs)
+
+def electrolyte_conductivity_EC_EMC_3_7_Landesfeind2019_Constant(c_e, T):
+    coeffs = np.array([5.21e-1, 2.28e2, -1.06, 3.53e-1, -3.59e-3, 1.48e-3])
+
+    return electrolyte_conductivity_base_Landesfeind2019_Constant(c_e, T, coeffs)
+
+def electrolyte_conductivity_base_Landesfeind2019(c_e, T, coeffs):
+    # mol.m-3 -> mol.l
+    c = c_e / 1000 # Mark Ruihe 
+    p1, p2, p3, p4, p5, p6 = coeffs
+    A = p1 * (1 + (T - p2))
+    B = 1 + p3 * pb.sqrt(c) + p4 * (1 + p5 * pb.exp(1000 / T)) * c
+    C = 1 + c ** 4 * (p6 * pb.exp(1000 / T))
     sigma_e = A * c * B / C  # mS.cm-1
 
     return sigma_e / 10
 
 def electrolyte_diffusivity_base_Landesfeind2019(c_e, T, coeffs):
-    c = c_e / 1000  # mol.m-3 -> mol.l
+    # mol.m-3 -> mol.l
+    c = c_e / 1000 # Mark Ruihe 
     p1, p2, p3, p4 = coeffs
-    A = p1 * pybamm.exp(p2 * c)
-    B = pybamm.exp(p3 / T)
-    C = pybamm.exp(p4 * c / T)
+    A = p1 * pb.exp(p2 * c)
+    B = pb.exp(p3 / T)
+    C = pb.exp(p4 * c / T)
     D_e = A * B * C * 1e-10  # m2/s
 
     return D_e
+
+def electrolyte_TDF_base_Landesfeind2019(c_e, T, coeffs):
+    c = c_e / 1000 # Mark Ruihe 
+    p1, p2, p3, p4, p5, p6, p7, p8, p9 = coeffs
+    tdf = (
+        p1
+        + p2 * c
+        + p3 * T
+        + p4 * c ** 2
+        + p5 * c * T
+        + p6 * T ** 2
+        + p7 * c ** 3
+        + p8 * c ** 2 * T
+        + p9 * c * T ** 2
+    )
+    return tdf
+
+def electrolyte_transference_number_base_Landesfeind2019(c_e, T, coeffs):
+    c = c_e / 1000 # Mark Ruihe 
+    p1, p2, p3, p4, p5, p6, p7, p8, p9 = coeffs
+    tplus = (
+        p1
+        + p2 * c
+        + p3 * T
+        + p4 * c ** 2
+        + p5 * c * T
+        + p6 * T ** 2
+        + p7 * c ** 3
+        + p8 * c ** 2 * T
+        + p9 * c * T ** 2
+    )
+
+    return tplus
+def electrolyte_transference_number_EC_EMC_3_7_Landesfeind2019(c_e, T):
+    coeffs = np.array(
+        [
+            -1.28e1,
+            -6.12,
+            8.21e-2,
+            9.04e-1,
+            3.18e-2,
+            -1.27e-4,
+            1.75e-2,
+            -3.12e-3,
+            -3.96e-5,
+        ]
+    )
+
+    return electrolyte_transference_number_base_Landesfeind2019(c_e, T, coeffs)
+
+def electrolyte_TDF_EC_EMC_3_7_Landesfeind2019(c_e, T):
+    coeffs = np.array(
+        [2.57e1, -4.51e1, -1.77e-1, 1.94, 2.95e-1, 3.08e-4, 2.59e-1, -9.46e-3, -4.54e-4]
+    )
+
+    return electrolyte_TDF_base_Landesfeind2019(c_e, T, coeffs)
+
 def electrolyte_diffusivity_EC_EMC_3_7_Landesfeind2019(c_e, T):
     coeffs = np.array([1.01e3, 1.01, -1.56e3, -4.87e2])
 
@@ -93,8 +241,8 @@ class TimeoutFunc(object):
         try:
             Result_List = q.get(timeout=self.timeout)
         except Empty as exc: 
-            # todo: need to incooperate other cases such as pybamm.expression_tree.exceptions.ModelError,
-            #      pybamm.expression_tree.exceptions.SolverError
+            # todo: need to incooperate other cases such as pb.expression_tree.exceptions.ModelError,
+            #      pb.expression_tree.exceptions.SolverError
             p.terminate()
             Call_ref = RioCallback() 
             Result_List = [
--- wip/Rio_Code/Text_copy.py
+++ wip/Rio_Code/Text_copy.py
@@ -1,23 +1,4 @@
-# define experiments-1 and output keys
-V_max = 4.2;        
-V_min = 2.5; 
-charge_time_mins = 60 * 4.86491/5
-exp_AGE_text = [(
-    f"Charge at 0.3 C for {charge_time_mins} minutes",
-    f"Discharge at 1 C until {V_min} V", 
-    ),  ]
-# step index for ageing
-step_AGE_CD =0;   step_AGE_CC =1;   step_AGE_CV =2;
-
-exp_RPT_text = [ (
-    f"Discharge at 0.1C until {V_min} V (5 minute period)",  
-    "Rest for 1 hours (5 minute period)",  
-    f"Charge at 0.1C until {V_max} V (5 minute period)",
-    ) ]
-# step index for RPT
-step_RPT_CD = 2;  step_RPT_RE =3;   step_RPT_CC = 4;  
-
-exp_text_list = [exp_AGE_text, exp_RPT_text,];
-cycle_no = -1; 
-exp_index_pack = [cycle_no,step_AGE_CD,step_AGE_CC,step_AGE_CV,
-   step_RPT_CD,step_RPT_RE , step_RPT_CC ];
+axs[0,0].plot(c_e, D_e_landes_AllT_cons[i],    '--',color=cm[i]) # label=f'Cycle {cycles[i]}'
+    axs[0,1].plot(c_e, sigma_e_landes_AllT_cons[i],    '--',color=cm[i]) 
+    axs[1,0].plot(c_e, t0plus_e_landes_AllT_cons[i],    '--',color=cm[i]) 
+    axs[1,1].plot(c_e, TDF_e_landes_AllT_cons[i],    '--',color=cm[i]) 
